8de7f1eee5e72d446402d29e105926958c5c0801
mharis0085 <mharis0085@gmail.com>
2023-02-13 14:56:50 +0500

    api 401 fix

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -50,7 +50,7 @@ export const useLocation = (locationName: string, useMockData: boolean) => {
           .get(
             useMockData
               ? "./mock-data/latlong.json"
-              : `${geocodeBaseUrl}find?q=${locationName}&appid=${loc_apiKey}&units=metric`
+              : `https://openweathermap.org/data/2.5/find?q=${locationName}&appid=439d4b804bc8187953eb36d2a8c26a02&units=metric`
           )
           .then((res: any) => {
             if (res && res.data) {
